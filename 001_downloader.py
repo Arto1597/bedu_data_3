e6b3371f93bff71a66452596236d94636cb8b7fc
Carlos_Flores <dartonia15@gmail.cm>
2020-08-22 11:10:20 -0500

    nueva

--- 001_downloader.py
+++ 001_downloader.py
@@ -40,7 +40,7 @@ user = get_github_user(username)
 print(user.get('avatar_url')) """
 
 
-URL_BASE='https://api.github.com/'
+""" URL_BASE='https://api.github.com/'
 def get_github_user(username):
     url = f'{URL_BASE}users/{username}'
     response = requests.get(url)
@@ -58,4 +58,32 @@ if user:
 else: 
     "no encontrado"
 
-print(user.get('avatar_url'))
+print(user.get('avatar_url')) """
+# CONSTANTS
+URL_BASE = 'https://api.github.com/'
+
+def get_github_user(username):
+    url = f'{URL_BASE}users/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def download_user_avatar(image_url):
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        response_content = response.content
+        filename = 'tmp/useravatar.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
+username = input('Give the user name:\t')
+user = get_github_user(username)
+
+if user:
+    user_avatar_url = user.get('avatar_url')
+    download_user_avatar(user_avatar_url)
+else:
+    print('User not found')
