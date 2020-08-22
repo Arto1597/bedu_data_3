6ac4d9a0fb416f4779bf1e1688b290f26446a2d5
Carlos_Flores <dartonia15@gmail.cm>
2020-08-22 13:52:20 -0500

    Puntos para tarea

---
+++ 005_employees_log.py
@@ -0,0 +1,8 @@
+# give 10,000 logs
+
+#group logs by employee
+
+#Number of each category "enter for" a employee has entered 
+#1.e. employee 1002 metting 52 , call - 80
+
+#show hot hours  for enter to the facilities
