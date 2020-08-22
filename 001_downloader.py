c5616d2c89f37c3ef916e20b9166fe93cc3c20d6
Carlos_Flores <dartonia15@gmail.cm>
2020-08-22 12:19:25 -0500

    add hm

--- 001_downloader.py
+++ 001_downloader.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 
 """ # CONSTANTS
@@ -60,28 +61,29 @@ else:
 
 print(user.get('avatar_url')) """
 # CONSTANTS
-URL_BASE = 'https://api.github.com/'
-
+URL_BASE='https://api.github.com/'
+# FUNTIONS
 def get_github_user(username):
     url = f'{URL_BASE}users/{username}'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     return None
-
 def download_user_avatar(image_url):
     response = requests.get(image_url)
     if response.status_code == 200:
         response_content = response.content
-        filename = 'tmp/useravatar.png'
+        filename = f'tmp/{image_filename()}.png'
         with open(filename, 'wb') as image:
             image.write(response_content)
             return filename
     return None
-
-username = input('Give the user name:\t')
+def image_filename():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    return timestamp
+username = input('Give the username:\t')
 user = get_github_user(username)
-
 if user:
     user_avatar_url = user.get('avatar_url')
     download_user_avatar(user_avatar_url)
